0e7628e3fa137823dfb3835f590b14bed4321b85
MillicentNj <millicent.njuguna@allangray.co.za>
2019-08-25 23:43:54 +0300

    [fix] production code

--- src/app/base-url.js
+++ src/app/base-url.js
@@ -1,6 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3002/"
-    : "https://bucketlist-node-api.herokuapp.com/";
+  process.env.NODE_ENV === "production"
+    ? "https://bucketlist-node-api.herokuapp.com/"
+    : "http://localhost:3002/";
 
 export default baseUrl;
