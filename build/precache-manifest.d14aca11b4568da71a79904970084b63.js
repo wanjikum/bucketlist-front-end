73e9e970c3bf19edccac8152a3a176cbdd3964bd
MillicentNj <millicent.njuguna@allangray.co.za>
2019-08-26 21:56:53 +0300

    none: push build folder

---
+++ build/precache-manifest.d14aca11b4568da71a79904970084b63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59631d788eb0f382ee8b640b03b98623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3385fb004e72f6d072a6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3385fb004e72f6d072a6",
+    "url": "/static/js/2.df2ac999.chunk.js"
+  },
+  {
+    "revision": "c7665f46c1ed71072f17",
+    "url": "/static/js/main.f89efd50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61c54f039e0837ee8d709b91887e6d3b",
+    "url": "/static/media/beauti-1.61c54f03.jpg"
+  }
+]);
